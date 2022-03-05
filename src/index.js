98b188e2b8c863d2b768ebdc1a632563a4ed0313
unknown <Khaledfarghly9991@gmail.com>
2022-03-05 15:21:48 +0200

    create the accordion for FQA section

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import "./styles/sass/main.scss";
 import handleRenderImages from "./js/renderImages";
 import handleFeatureSectionChanges from "./js/hadnleFeaturesChanges";
 import { hadnleHeaderchanges } from "./js/handleHeader";
+import { handleAccordionsChnges } from "./js/handleAccordion";
 handleRenderImages();
 hadnleHeaderchanges();
 handleFeatureSectionChanges();
+handleAccordionsChnges();
---
+++ src/js/handleAccordion.js
@@ -0,0 +1,16 @@
+import { addClassName, removeClassName } from "./helpers";
+const accordions = document.querySelector(".fqa__questions");
+
+function hadnleOpenAccordion(event) {
+  const targetItem = event.target.closest("h4");
+  if (!targetItem) {
+    return;
+  } else {
+    targetItem.classList.toggle("active");
+  }
+}
+function handleAccordionsChnges() {
+  accordions.addEventListener("click", hadnleOpenAccordion);
+}
+
+export { handleAccordionsChnges };
